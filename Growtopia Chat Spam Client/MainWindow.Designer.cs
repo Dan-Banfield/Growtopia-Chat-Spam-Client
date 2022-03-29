883076989c917d014c9f1e9cdda32c2ba0646e90
Dan Banfield <dandbanfield@gmail.com>
2022-03-29 17:09:34 +0100

    Added the GitHub logo, settings preperation and some UI event handler code.

--- Growtopia Chat Spam Client/MainWindow.Designer.cs
+++ Growtopia Chat Spam Client/MainWindow.Designer.cs
@@ -31,19 +31,22 @@ namespace Growtopia_Chat_Spam_Client
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             this.netSealTheme = new NSTheme();
-            this.closeButton = new NSControlButton();
-            this.minimizeButton = new NSControlButton();
             this.optionsGroupBox = new NSGroupBox();
-            this.titleLabel = new NSLabel();
-            this.textToSpamTextBox = new NSTextBox();
-            this.startSpamButton = new NSButton();
-            this.clearTextBoxButton = new NSButton();
-            this.topMostCheckBox = new NSCheckBox();
-            this.spamIntervalTrackBar = new NSTrackBar();
-            this.titleLabel2 = new NSLabel();
             this.selectedSpamIntervalLabel = new System.Windows.Forms.Label();
+            this.titleLabel2 = new NSLabel();
+            this.spamIntervalTrackBar = new NSTrackBar();
+            this.topMostCheckBox = new NSCheckBox();
+            this.clearTextBoxButton = new NSButton();
+            this.startSpamButton = new NSButton();
+            this.textToSpamTextBox = new NSTextBox();
+            this.titleLabel = new NSLabel();
+            this.minimizeButton = new NSControlButton();
+            this.closeButton = new NSControlButton();
+            this.githubLogoPictureBox = new System.Windows.Forms.PictureBox();
+            this.saveConfigButton = new NSButton();
             this.netSealTheme.SuspendLayout();
             this.optionsGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.githubLogoPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // netSealTheme
@@ -74,37 +77,13 @@ namespace Growtopia_Chat_Spam_Client
             this.netSealTheme.TransparencyKey = System.Drawing.Color.Empty;
             this.netSealTheme.Transparent = false;
             // 
-            // closeButton
-            // 
-            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.closeButton.ControlButton = NSControlButton.Button.Close;
-            this.closeButton.Location = new System.Drawing.Point(575, 4);
-            this.closeButton.Margin = new System.Windows.Forms.Padding(0);
-            this.closeButton.MaximumSize = new System.Drawing.Size(18, 20);
-            this.closeButton.MinimumSize = new System.Drawing.Size(18, 20);
-            this.closeButton.Name = "closeButton";
-            this.closeButton.Size = new System.Drawing.Size(18, 20);
-            this.closeButton.TabIndex = 0;
-            this.closeButton.Text = "Close";
-            // 
-            // minimizeButton
-            // 
-            this.minimizeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.minimizeButton.ControlButton = NSControlButton.Button.Minimize;
-            this.minimizeButton.Location = new System.Drawing.Point(548, 4);
-            this.minimizeButton.Margin = new System.Windows.Forms.Padding(0);
-            this.minimizeButton.MaximumSize = new System.Drawing.Size(18, 20);
-            this.minimizeButton.MinimumSize = new System.Drawing.Size(18, 20);
-            this.minimizeButton.Name = "minimizeButton";
-            this.minimizeButton.Size = new System.Drawing.Size(18, 20);
-            this.minimizeButton.TabIndex = 1;
-            this.minimizeButton.Text = "Minimize";
-            // 
             // optionsGroupBox
             // 
             this.optionsGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.optionsGroupBox.Controls.Add(this.saveConfigButton);
+            this.optionsGroupBox.Controls.Add(this.githubLogoPictureBox);
             this.optionsGroupBox.Controls.Add(this.selectedSpamIntervalLabel);
             this.optionsGroupBox.Controls.Add(this.titleLabel2);
             this.optionsGroupBox.Controls.Add(this.spamIntervalTrackBar);
@@ -122,65 +101,28 @@ namespace Growtopia_Chat_Spam_Client
             this.optionsGroupBox.Text = "nsGroupBox1";
             this.optionsGroupBox.Title = "Options";
             // 
-            // titleLabel
+            // selectedSpamIntervalLabel
             // 
-            this.titleLabel.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
-            this.titleLabel.Location = new System.Drawing.Point(17, 48);
-            this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(112, 23);
-            this.titleLabel.TabIndex = 0;
-            this.titleLabel.Text = "nsLabel1";
-            this.titleLabel.Value1 = "TEXT";
-            this.titleLabel.Value2 = " TO SPAM";
+            this.selectedSpamIntervalLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.selectedSpamIntervalLabel.AutoSize = true;
+            this.selectedSpamIntervalLabel.ForeColor = System.Drawing.Color.White;
+            this.selectedSpamIntervalLabel.Location = new System.Drawing.Point(302, 97);
+            this.selectedSpamIntervalLabel.Name = "selectedSpamIntervalLabel";
+            this.selectedSpamIntervalLabel.Size = new System.Drawing.Size(189, 13);
+            this.selectedSpamIntervalLabel.TabIndex = 7;
+            this.selectedSpamIntervalLabel.Text = "Selected spam interval: 100ms.";
             // 
-            // textToSpamTextBox
+            // titleLabel2
             // 
-            this.textToSpamTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textToSpamTextBox.AutoScroll = false;
-            this.textToSpamTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.textToSpamTextBox.Location = new System.Drawing.Point(17, 73);
-            this.textToSpamTextBox.MaxLength = 32767;
-            this.textToSpamTextBox.Multiline = false;
-            this.textToSpamTextBox.Name = "textToSpamTextBox";
-            this.textToSpamTextBox.NumberBounds = new System.Drawing.Point(0, 0);
-            this.textToSpamTextBox.NumbersOnly = false;
-            this.textToSpamTextBox.ReadOnly = false;
-            this.textToSpamTextBox.Size = new System.Drawing.Size(263, 193);
-            this.textToSpamTextBox.TabIndex = 1;
-            this.textToSpamTextBox.Text = "Your text to spam here.";
-            this.textToSpamTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.textToSpamTextBox.UseSystemPasswordChar = false;
-            // 
-            // startSpamButton
-            // 
-            this.startSpamButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.startSpamButton.Location = new System.Drawing.Point(305, 200);
-            this.startSpamButton.Name = "startSpamButton";
-            this.startSpamButton.Size = new System.Drawing.Size(225, 32);
-            this.startSpamButton.TabIndex = 2;
-            this.startSpamButton.Text = "Start spamming!";
-            // 
-            // clearTextBoxButton
-            // 
-            this.clearTextBoxButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.clearTextBoxButton.Location = new System.Drawing.Point(305, 162);
-            this.clearTextBoxButton.Name = "clearTextBoxButton";
-            this.clearTextBoxButton.Size = new System.Drawing.Size(225, 32);
-            this.clearTextBoxButton.TabIndex = 3;
-            this.clearTextBoxButton.Text = "Clear text box";
-            this.clearTextBoxButton.Click += new System.EventHandler(this.clearTextBoxButton_Click);
-            // 
-            // topMostCheckBox
-            // 
-            this.topMostCheckBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.topMostCheckBox.Checked = false;
-            this.topMostCheckBox.Location = new System.Drawing.Point(449, 243);
-            this.topMostCheckBox.Name = "topMostCheckBox";
-            this.topMostCheckBox.Size = new System.Drawing.Size(81, 23);
-            this.topMostCheckBox.TabIndex = 4;
-            this.topMostCheckBox.Text = "Top most";
+            this.titleLabel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.titleLabel2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
+            this.titleLabel2.Location = new System.Drawing.Point(305, 71);
+            this.titleLabel2.Name = "titleLabel2";
+            this.titleLabel2.Size = new System.Drawing.Size(127, 23);
+            this.titleLabel2.TabIndex = 6;
+            this.titleLabel2.Text = "nsLabel1";
+            this.titleLabel2.Value1 = "SPAM";
+            this.titleLabel2.Value2 = " INTERVAL";
             // 
             // spamIntervalTrackBar
             // 
@@ -195,28 +137,112 @@ namespace Growtopia_Chat_Spam_Client
             this.spamIntervalTrackBar.Value = 100;
             this.spamIntervalTrackBar.Scroll += new NSTrackBar.ScrollEventHandler(this.spamIntervalTrackBar_Scroll);
             // 
-            // titleLabel2
+            // topMostCheckBox
             // 
-            this.titleLabel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.titleLabel2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
-            this.titleLabel2.Location = new System.Drawing.Point(305, 71);
-            this.titleLabel2.Name = "titleLabel2";
-            this.titleLabel2.Size = new System.Drawing.Size(127, 23);
-            this.titleLabel2.TabIndex = 6;
-            this.titleLabel2.Text = "nsLabel1";
-            this.titleLabel2.Value1 = "SPAM";
-            this.titleLabel2.Value2 = " INTERVAL";
+            this.topMostCheckBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.topMostCheckBox.Checked = false;
+            this.topMostCheckBox.Location = new System.Drawing.Point(449, 243);
+            this.topMostCheckBox.Name = "topMostCheckBox";
+            this.topMostCheckBox.Size = new System.Drawing.Size(81, 23);
+            this.topMostCheckBox.TabIndex = 4;
+            this.topMostCheckBox.Text = "Top most";
+            this.topMostCheckBox.CheckedChanged += new NSCheckBox.CheckedChangedEventHandler(this.topMostCheckBox_CheckedChanged);
             // 
-            // selectedSpamIntervalLabel
+            // clearTextBoxButton
             // 
-            this.selectedSpamIntervalLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.selectedSpamIntervalLabel.AutoSize = true;
-            this.selectedSpamIntervalLabel.ForeColor = System.Drawing.Color.White;
-            this.selectedSpamIntervalLabel.Location = new System.Drawing.Point(302, 97);
-            this.selectedSpamIntervalLabel.Name = "selectedSpamIntervalLabel";
-            this.selectedSpamIntervalLabel.Size = new System.Drawing.Size(189, 13);
-            this.selectedSpamIntervalLabel.TabIndex = 7;
-            this.selectedSpamIntervalLabel.Text = "Selected spam interval: 100ms.";
+            this.clearTextBoxButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.clearTextBoxButton.Location = new System.Drawing.Point(305, 162);
+            this.clearTextBoxButton.Name = "clearTextBoxButton";
+            this.clearTextBoxButton.Size = new System.Drawing.Size(225, 32);
+            this.clearTextBoxButton.TabIndex = 3;
+            this.clearTextBoxButton.Text = "Clear text box";
+            this.clearTextBoxButton.Click += new System.EventHandler(this.clearTextBoxButton_Click);
+            // 
+            // startSpamButton
+            // 
+            this.startSpamButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.startSpamButton.Location = new System.Drawing.Point(305, 200);
+            this.startSpamButton.Name = "startSpamButton";
+            this.startSpamButton.Size = new System.Drawing.Size(225, 32);
+            this.startSpamButton.TabIndex = 2;
+            this.startSpamButton.Text = "Start spamming!";
+            // 
+            // textToSpamTextBox
+            // 
+            this.textToSpamTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textToSpamTextBox.AutoScroll = false;
+            this.textToSpamTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textToSpamTextBox.Location = new System.Drawing.Point(17, 73);
+            this.textToSpamTextBox.MaxLength = 32767;
+            this.textToSpamTextBox.Multiline = true;
+            this.textToSpamTextBox.Name = "textToSpamTextBox";
+            this.textToSpamTextBox.NumberBounds = new System.Drawing.Point(0, 0);
+            this.textToSpamTextBox.NumbersOnly = false;
+            this.textToSpamTextBox.ReadOnly = false;
+            this.textToSpamTextBox.Size = new System.Drawing.Size(263, 193);
+            this.textToSpamTextBox.TabIndex = 1;
+            this.textToSpamTextBox.Text = "Your text to spam here.";
+            this.textToSpamTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.textToSpamTextBox.UseSystemPasswordChar = false;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
+            this.titleLabel.Location = new System.Drawing.Point(17, 48);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(112, 23);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "nsLabel1";
+            this.titleLabel.Value1 = "TEXT";
+            this.titleLabel.Value2 = " TO SPAM";
+            // 
+            // minimizeButton
+            // 
+            this.minimizeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.minimizeButton.ControlButton = NSControlButton.Button.Minimize;
+            this.minimizeButton.Location = new System.Drawing.Point(548, 4);
+            this.minimizeButton.Margin = new System.Windows.Forms.Padding(0);
+            this.minimizeButton.MaximumSize = new System.Drawing.Size(18, 20);
+            this.minimizeButton.MinimumSize = new System.Drawing.Size(18, 20);
+            this.minimizeButton.Name = "minimizeButton";
+            this.minimizeButton.Size = new System.Drawing.Size(18, 20);
+            this.minimizeButton.TabIndex = 1;
+            this.minimizeButton.Text = "Minimize";
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeButton.ControlButton = NSControlButton.Button.Close;
+            this.closeButton.Location = new System.Drawing.Point(575, 4);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(0);
+            this.closeButton.MaximumSize = new System.Drawing.Size(18, 20);
+            this.closeButton.MinimumSize = new System.Drawing.Size(18, 20);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(18, 20);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.Text = "Close";
+            // 
+            // githubLogoPictureBox
+            // 
+            this.githubLogoPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.githubLogoPictureBox.Image = global::Growtopia_Chat_Spam_Client.Properties.Resources.GitHub_Mark_Light_120px_plus;
+            this.githubLogoPictureBox.Location = new System.Drawing.Point(443, 12);
+            this.githubLogoPictureBox.Name = "githubLogoPictureBox";
+            this.githubLogoPictureBox.Size = new System.Drawing.Size(100, 50);
+            this.githubLogoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.githubLogoPictureBox.TabIndex = 8;
+            this.githubLogoPictureBox.TabStop = false;
+            // 
+            // saveConfigButton
+            // 
+            this.saveConfigButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveConfigButton.Location = new System.Drawing.Point(305, 241);
+            this.saveConfigButton.Name = "saveConfigButton";
+            this.saveConfigButton.Size = new System.Drawing.Size(138, 27);
+            this.saveConfigButton.TabIndex = 9;
+            this.saveConfigButton.Text = "Save config";
             // 
             // MainWindow
             // 
@@ -234,6 +260,7 @@ namespace Growtopia_Chat_Spam_Client
             this.netSealTheme.ResumeLayout(false);
             this.optionsGroupBox.ResumeLayout(false);
             this.optionsGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.githubLogoPictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -252,6 +279,8 @@ namespace Growtopia_Chat_Spam_Client
         private NSTrackBar spamIntervalTrackBar;
         private NSLabel titleLabel2;
         private System.Windows.Forms.Label selectedSpamIntervalLabel;
+        private System.Windows.Forms.PictureBox githubLogoPictureBox;
+        private NSButton saveConfigButton;
     }
 }
 
--- Growtopia Chat Spam Client/MainWindow.cs
+++ Growtopia Chat Spam Client/MainWindow.cs
@@ -8,6 +8,8 @@ namespace Growtopia_Chat_Spam_Client
         public MainWindow()
         {
             InitializeComponent();
+
+            Properties.Settings.Default.PropertyChanged += (o, e) => Properties.Settings.Default.Save();
         }
 
         #region UI
@@ -17,6 +19,19 @@ namespace Growtopia_Chat_Spam_Client
         private void clearTextBoxButton_Click(object sender, EventArgs e) => textToSpamTextBox.Text = "";
 
         private void spamIntervalTrackBar_Scroll(object sender) => selectedSpamIntervalLabel.Text = $"Selected spam interval: {spamIntervalTrackBar.Value}ms.";
+
+        private void topMostCheckBox_CheckedChanged(object sender)
+        {
+            switch (topMostCheckBox.Checked)
+            {
+                case true:
+                    this.TopMost = true;
+                    break;
+                case false:
+                    this.TopMost = false;
+                    break;
+            }
+        }
 
         #endregion
 
